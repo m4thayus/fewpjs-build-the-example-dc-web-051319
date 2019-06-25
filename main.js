f947effa44ed638f9a7dc8e4d034044975ff9fd9
Matt Williams <matt.seongjun@gmail.com>
2019-06-25 15:11:10 -0400

    Done

--- main.js
+++ main.js
@@ -2,10 +2,47 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+// error message toggles
+let errorModal =  document.getElementById("modal");
 
+function hideError() {
+    errorModal.className = "hidden";
+};
 
+function showErrorModal() {
+    errorModal.className = "show";
+};
 
+// Hide error-modal on load
+hideError();
+
+// get posts to iterate over for likes
+let posts = document.getElementsByClassName("media-post");
+
+function changeHeart(post) {
+    let heart = post.querySelector(".like-glyph");
+    if (heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+    } else {
+        heart.innerText = EMPTY_HEART;
+    }
+};
+
+function showError(message) {
+    showErrorModal();
+    document.getElementById("modal-message").innerText = message;
+    // setTimeout function doesn't like ()
+    window.setTimeout(hideError, 5000);
+};
+
+for (let i = 0; i < posts.length; i++ ) {
+    let like = posts[i].querySelector(".like");
+    like.addEventListener('click', (event) => {
+        mimicServerCall()
+            .then(changeHeart(posts[i]))
+            .catch( (error) => {showError(error);} )
+    });
+};
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
